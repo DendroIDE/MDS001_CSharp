4e4333110eec6145aab1c962d73e0f281c638467
DendroIDE <dendroide.code@gmail.com>
2024-04-16 23:59:23 -0500

    Comentarios en el metodo para obtener la cadena de conexion desde un archivo xml

--- LP02_ConexionBD/C01_Oracle/MetodosConexion.cs
+++ LP02_ConexionBD/C01_Oracle/MetodosConexion.cs
@@ -13,26 +13,28 @@ namespace LP02_ConexionBD.C01_Oracle
             string Ensamblador_Name = Assembly.GetExecutingAssembly().Location;
             string Directorio_Name = Path.GetDirectoryName(Ensamblador_Name);
             string rutaDocumentoXML = Path.Combine(Directorio_Name, nombreCarpetaRecursos, nombreArchivoXML);
-            //Variable de conteo del número de nodos repetidos
-            int conteoNodosConexionesRepetidas = 0;
+            //Variable que permiten la lectura de los elementos del documento XML que contiene las cadenas de conexión.
+            XElement root = XElement.Load(rutaDocumentoXML);
+            //Variables que representan los elementos del xml.
+            IEnumerable<XElement> address;
+            IEnumerable<XElement> entorno;
             //Cargar el documento XML
             try
             {
                 //Crear la instancia de xmldocument
                 XmlDocument doc = new XmlDocument();
                 doc.Load(rutaDocumentoXML);
-                // Si tiene nodos, recorrer sus hijos
+                // Si tiene nodos y el elemento root se define como <cadena_conexion> </cadena_conexion>, recorrer sus hijos
                 if (doc.DocumentElement.HasChildNodes && doc.DocumentElement.Name.Equals("cadena_conexion"))
-                {
-                    XElement root = XElement.Load(rutaDocumentoXML);
-                    IEnumerable<XElement> address =
+                {                    
+                    address =
                         from elemento in root.Elements("conexion")
                         where (string)elemento.Attribute("base_datos") == "oracle"
                         select elemento;
 
                     if (address.Count() == 1)
                     {
-                        IEnumerable<XElement> entorno =
+                        entorno =
                             from element in address.Elements("variable_cadena_conexion")
                             where (string) element.Attribute("entorno_empresarial") == "MEGAKONS"
                             select element;
@@ -60,11 +62,14 @@ namespace LP02_ConexionBD.C01_Oracle
                 }
                 else
                 {
+
+                    // Imprimir error del documento <cadena_conexion> </cadena_conexion>.
                     Console.WriteLine("Revisar archivo de conexión XML elemento <cadena_conexion>.");
                 }
             }
             catch (Exception e)
             {
+                //Imprimir cualquiera error surgiente al ejecutar la lectura del archivo de cadena de conexion
                 Console.WriteLine(e.Message);
             }
             return "";
