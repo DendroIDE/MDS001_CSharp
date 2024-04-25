124cbad747b740f806bd1a18930da16492d71ab9
DendroIDE Code <dendroide.code@gmail.com>
2024-04-25 18:38:30 -0500

    Dependency Injection 2

---
+++ LP06_DependencyInjection/DI02_MetodologiaCompleta/Services/FakeTodoItemService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP06_DependencyInjection.DI02_MetodologiaCompleta.Services
+{
+    internal class FakeTodoItemService : ITodoItemService
+    {
+        public Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var item1 = new TodoItem
+            {
+                Title = "Learn ASP.NET Core",
+                DueAt = DateTimeOffset.Now.AddDays(1)
+            };
+
+            var item2 = new TodoItem
+            {
+                Title = "Build awesome apps",
+                DueAt = DateTimeOffset.Now.AddDays(2)
+            };
+
+            return Task.FromResult(new[] { item1, item2 });
+        }
+    }
+}
---
+++ LP06_DependencyInjection/DI02_MetodologiaCompleta/Services/TodoController .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP06_DependencyInjection.DI02_MetodologiaCompleta.Services
+{
+    internal class TodoController
+    {
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public void Index()
+        {
+            // Obtener las tareas desde la base de datos
+
+            // Coloca los elemento dentro de un modelo
+
+            // Pasa la vista al model y visualiza
+        }
+    }
+}
