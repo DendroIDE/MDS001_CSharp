90fff6aec7cde48477db3a74681aab878833d23e
DendroIDE <dendroide.code@gmail.com>
2024-05-21 02:03:25 -0500

    Configuracion inicial completa NLOG

---
+++ LP07_Logs/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LP07_Logs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Ambato", "Quito"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Hello, this is the index!");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ LP07_Logs/Program.cs
@@ -0,0 +1,59 @@
+
+using NLog;
+using NLog.Web;
+using System;
+
+namespace LP07_Logs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Init temprano de NLog para permitir el inicio y el registro de excepciones, antes de que se construya el host
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("init main");
+
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Añadir servicios al contenedor.
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+                //Limpieza de proveedores y contrucción del Host para utilización de NLog
+                builder.Logging.ClearProviders();
+                // NLog: Configurar NLog para inyección de dependencias
+                builder.Host.UseNLog();
+
+                var app = builder.Build();
+
+                // Configurar el canal de peticiones HTTP.
+                if (app.Environment.IsProduction())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception exception)
+            {
+                // NLog: detectar errores de configuración
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Asegúrese de vaciar y detener los temporizadores/hilos internos antes de salir de la aplicación (Evite el fallo de segmentación en Linux)
+                NLog.LogManager.Shutdown();
+            }
+        }
+    }
+}
